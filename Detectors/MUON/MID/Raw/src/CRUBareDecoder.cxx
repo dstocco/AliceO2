6e989b14da1eb4c3c4733c17a6c9152bda76858e
dstocco <diego.stocco@cern.ch>
2020-03-13 13:22:41 +0100

    Improve the decoding of MID data obtained without the user logic
    - Use one decoder per GBT
    - Cope with delayed RO data compared to RDH
    - Introduce a delay between regional and local clocks
    - Better separation between debug and production mode
    - Fix calibration clock initialization
    - Speed-up the reading of the raw buffer

--- Detectors/MUON/MID/Raw/src/CRUBareDecoder.cxx
+++ Detectors/MUON/MID/Raw/src/CRUBareDecoder.cxx
@@ -29,12 +29,12 @@ void CRUBareDecoder::reset()
   mROFRecords.clear();
 }
 
-void CRUBareDecoder::init(bool debugMode)
+void CRUBareDecoder::init(const CrateFeeIdMapper& feeIdMapper, const CrateMasks& masks, bool debugMode)
 {
   /// Initializes the task
-  if (debugMode == true) {
-    mAddReg = std::bind(&CRUBareDecoder::addBoard, this, std::placeholders::_1);
-    mCheckBoard = [](size_t) { return true; };
+  mCrateFeeIdMapper = feeIdMapper;
+  for (uint16_t igbt = 0; igbt < crateparams::sNGBTs; ++igbt) {
+    mGBTDecoders[igbt].init(igbt, masks.getMask(igbt), debugMode);
   }
 }
 
@@ -43,103 +43,25 @@ void CRUBareDecoder::process(gsl::span<const uint8_t> bytes)
   /// Decodes the buffer
   reset();
 
-  mBuffer.setBuffer(bytes);
-
-  // Each CRU word consists of 128 bits, i.e 16 bytes
-  while (mBuffer.hasNext(16)) {
-    processGBT(0);
-    processGBT(5);
-    // The GBT word consists of 10 bytes
-    // but the CRU word is 16 bytes
-    // So we have 6 empty bytes
-    for (int iword = 0; iword < 6; ++iword) {
-      mBuffer.next();
+  mHandler.setBuffer(bytes);
+  while (mHandler.nextNonEmptyHBF()) {
+    uint16_t feeId = mCrateFeeIdMapper.getFeeId(mHandler.getRDH()->linkID, mHandler.getRDH()->endPointID, mHandler.getRDH()->cruID);
+    mGBTDecoders[feeId].process(mHandler.getPayload(), *mHandler.getRDH());
+    size_t firstEntry = mData.size();
+    mData.insert(mData.end(), mGBTDecoders[feeId].getData().begin(), mGBTDecoders[feeId].getData().end());
+    size_t lastRof = mROFRecords.size();
+    mROFRecords.insert(mROFRecords.end(), mGBTDecoders[feeId].getROFRecords().begin(), mGBTDecoders[feeId].getROFRecords().end());
+    for (auto rofIt = mROFRecords.begin() + lastRof; rofIt != mROFRecords.end(); ++rofIt) {
+      rofIt->firstEntry += firstEntry;
     }
   }
-}
-
-void CRUBareDecoder::processGBT(size_t offset)
-{
-  /// Processes the GBT
-
-  // Regional link
-  // loc#0 loc#1 loc#2 loc#3 reg
-  std::array<uint8_t, 5> bytes{mBuffer.next(), mBuffer.next(), mBuffer.next(), mBuffer.next(), mBuffer.next()};
-
-  // Byte corresponding to regional
-  size_t ibyte = 4;
-  size_t ilink = offset + ibyte;
-
-  if (mELinkDecoders[ilink].add(bytes[ibyte], 0x80) && mELinkDecoders[ilink].isComplete()) {
-    // In principle, in the same HB we should have the info of only 1 crate
-    // So it should be safe to store this information
-    // and apply it to all subsequent e-links, without checking the clock
-    mCrateId = mELinkDecoders[ilink].getId();
-    mAddReg(ilink);
-    mELinkDecoders[ilink].reset();
-  }
-
-  // local links
-  for (ibyte = 0; ibyte < 4; ++ibyte) {
-    ilink = offset + ibyte;
-    if (mELinkDecoders[ilink].add(bytes[ibyte], 0xc0) && mELinkDecoders[ilink].isComplete()) {
-      if (mCheckBoard(ilink)) {
-        addLoc(ilink);
-      }
-      mELinkDecoders[ilink].reset();
-    }
-  }
-}
-
-bool CRUBareDecoder::checkBoard(size_t ilink)
-{
-  /// Performs checks on the board
-  uint8_t expectedId = ilink - (ilink / 5);
-  return (expectedId == mELinkDecoders[ilink].getId() % 8);
-}
-
-void CRUBareDecoder::addBoard(size_t ilink)
-{
-  /// Adds the local or regional board to the output data vector
-  uint16_t localClock = mELinkDecoders[ilink].getCounter();
-  EventType eventType = EventType::Standard;
-  if (mELinkDecoders[ilink].getTriggerWord() & (1 << 4)) {
-    mCalibClocks[ilink] = localClock;
-    eventType = EventType::Noise;
-  } else if (localClock == mCalibClocks[ilink] + sDelayCalibToFET) {
-    eventType = EventType::Dead;
-  }
-  auto firstEntry = mData.size();
-  InteractionRecord intRec(localClock - sDelayBCToLocal, mBuffer.getRDH()->triggerOrbit);
-  mData.push_back({mELinkDecoders[ilink].getStatusWord(), mELinkDecoders[ilink].getTriggerWord(), crateparams::makeUniqueLocID(mCrateId, mELinkDecoders[ilink].getId()), mELinkDecoders[ilink].getInputs()});
-  mROFRecords.emplace_back(intRec, eventType, firstEntry, 1);
-}
-
-void CRUBareDecoder::addLoc(size_t ilink)
-{
-  /// Adds the local board to the output data vector
-  addBoard(ilink);
-  bool invert = (mROFRecords.back().eventType == EventType::Dead);
-  for (int ich = 0; ich < 4; ++ich) {
-    if ((mData.back().firedChambers & (1 << ich))) {
-      mData.back().patternsBP[ich] = getPattern(mELinkDecoders[ilink].getPattern(0, ich), invert);
-      mData.back().patternsNBP[ich] = getPattern(mELinkDecoders[ilink].getPattern(1, ich), invert);
-    }
-  }
-  mELinkDecoders[ilink].reset();
-}
-
-uint16_t CRUBareDecoder::getPattern(uint16_t pattern, bool invert) const
-{
-  /// Gets the proper pattern
-  return (invert) ? ~pattern : pattern;
 }
 
 bool CRUBareDecoder::isComplete() const
 {
   /// Checks that all links have finished reading
-  for (auto& elink : mELinkDecoders) {
-    if (elink.getNBytes() > 0) {
+  for (auto& decoder : mGBTDecoders) {
+    if (!decoder.isComplete()) {
       return false;
     }
   }
