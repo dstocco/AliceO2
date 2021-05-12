aabe5a5b5675c53c14bc6d28d8d902c3da2b0146
dstocco <diego.stocco@cern.ch>
2021-05-12 17:26:41 +0200

    Workflow to produce masks for MID

---
+++ Detectors/MUON/MID/Workflow/include/MIDWorkflow/MaskMakerSpec.h
@@ -0,0 +1,32 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file   MIDWorkflow/MaskMakerSpec.h
+/// \brief  Processor to compute the masks
+/// \author Diego Stocco <Diego.Stocco at cern.ch>
+/// \date   12 may 2021
+
+#ifndef O2_MID_MASKMAKERSPEC_H
+#define O2_MID_MASKMAKERSPEC_H
+
+#include "Framework/DataProcessorSpec.h"
+
+#include "MIDRaw/CrateMasks.h"
+#include "MIDRaw/FEEIdConfig.h"
+
+namespace o2
+{
+namespace mid
+{
+framework::DataProcessorSpec getMaskMakerSpec(const FEEIdConfig& feeIdConfig, const CrateMasks& crateMasks);
+}
+} // namespace o2
+
+#endif //O2_MID_MASKMAKERSPEC_H
---
+++ Detectors/MUON/MID/Workflow/src/mask-maker-workflow.cxx
@@ -0,0 +1,52 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file   MID/Workflow/src/decoded-digits-writer-workflow.cxx
+/// \brief  MID decoded digits writer workflow
+/// \author Diego Stocco <Diego.Stocco at cern.ch>
+/// \date   29 October 2020
+
+#include <string>
+#include <vector>
+#include "Framework/Variant.h"
+#include "Framework/ConfigParamSpec.h"
+
+using namespace o2::framework;
+
+void customize(std::vector<ConfigParamSpec>& workflowOptions)
+{
+  std::vector<ConfigParamSpec> options{
+    {"feeId-config-file", VariantType::String, "", {"Filename with crate FEE ID correspondence"}},
+    {"crate-masks-file", VariantType::String, "", {"Filename with crate masks"}}};
+  workflowOptions.insert(workflowOptions.end(), options.begin(), options.end());
+}
+
+#include "Framework/runDataProcessing.h"
+#include "MIDWorkflow/MaskMakerSpec.h"
+#include "MIDWorkflow/MaskHandlerSpec.h"
+
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
+{
+  auto feeIdConfigFilename = cfgc.options().get<std::string>("feeId-config-file");
+  o2::mid::FEEIdConfig feeIdConfig;
+  if (!feeIdConfigFilename.empty()) {
+    feeIdConfig = o2::mid::FEEIdConfig(feeIdConfigFilename.c_str());
+  }
+  auto crateMasksFilename = cfgc.options().get<std::string>("crate-masks-file");
+  o2::mid::CrateMasks crateMasks;
+  if (!crateMasksFilename.empty()) {
+    crateMasks = o2::mid::CrateMasks(crateMasksFilename.c_str());
+  }
+  WorkflowSpec specs;
+  specs.emplace_back(o2::mid::getMaskMakerSpec(feeIdConfig, crateMasks));
+  specs.emplace_back(o2::mid::getMaskHandlerSpec());
+
+  return specs;
+}
